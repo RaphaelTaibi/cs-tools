4537ba1ca2259d73c086d9d8194669a335655d52
Raphael Taibi <raphael.taibi@epitech.eu>
2025-10-13 22:56:50 +0200

    feat: :sparkles: Add Hooks
    Add Hooks feature like debounce for performance LocalStrorage & Toggle

---
+++ src/lib/hooks/index.ts
@@ -0,0 +1,8 @@
+// Export all hooks
+export { default as useLocalStorage } from './useLocalStorage.ts';
+export { default as useDebounce } from './useDebounce.ts';
+export { default as useToggle } from './useToggle.ts';
+
+// Export hook types
+export type { UseLocalStorageReturn } from './useLocalStorage.ts';
+export type { UseToggleReturn } from './useToggle.ts';
