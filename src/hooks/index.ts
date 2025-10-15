02f7782bde845241944721808c214035b08a3d54
Raphael Taibi <raphael.taibi@epitech.eu>
2025-10-15 22:53:58 +0200

    fix: :bug: Fix

--- src/hooks/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Hooks index file - exports all custom React hooks
- */
-
-export { useLocalStorage } from './useLocalStorage';
-export { useDebounce } from './useDebounce';
-export { useWindowSize } from './useWindowSize';
-export { useOnlineStatus } from './useOnlineStatus';
--- src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Utility index file - exports all utility functions
- */
-
-export * from './string';
-export * from './array';
-export * from './object';
