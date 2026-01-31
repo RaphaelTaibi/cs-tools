613804d7507c255b2996d7a03aec09833575e6ed
Raphael Taibi <raphael.taibi@epitech.eu>
2026-01-31 01:13:06 +0100

    retry react test fix CI

--- packages/react/vitest.setup.ts
+++ packages/react/vitest.setup.ts
@@ -1,4 +1,6 @@
-import '@testing-library/jest-dom/vitest';
+import * as matchers from '@testing-library/jest-dom/matchers';
+import { expect } from 'vitest';
+expect.extend(matchers);
 // Debug: vérifier si le setup est exécuté dans CI
 console.info('vitest.setup.ts loaded');
 
